4250a00fffc68be77f6c944e0fa457d37da90139
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-04-23 23:55:01 +0800

    move btn-signout to component

--- src/js/btn-signout/btn-signout.directive.js
+++ src/js/btn-signout/btn-signout.directive.js
@@ -1,18 +1,12 @@
 'use strict';
 
-angular.module('adama-mobile').directive('btnSignout', function() {
-	return {
-		templateUrl: 'adama-mobile/btn-signout/btn-signout.html',
-		restrict: 'E',
-		scope: {},
-		bindToController: {},
-		controller: function(Auth, $state) {
-			var ctrl = this;
-			ctrl.signout = function() {
-				Auth.logout();
-				$state.go('auth.signin');
-			};
-		},
-		controllerAs: 'ctrl'
-	};
+angular.module('adama-mobile').component('btnSignout', {
+	templateUrl : 'adama-mobile/btn-signout/btn-signout.html',
+	controller : function(Auth, $state) {
+		var ctrl = this;
+		ctrl.signout = function() {
+			Auth.logout();
+			$state.go('auth.signin');
+		};
+	}
 });
