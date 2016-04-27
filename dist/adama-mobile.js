9e9536cfba942efca3787dfb8986a628ce53a166
Vincent Delacourt <vincent.delacourt@adama-development.com>
2016-04-27 14:52:06 +0800

    change header name

--- dist/adama-mobile.js
+++ dist/adama-mobile.js
@@ -455,7 +455,7 @@ angular.module('adama-mobile').factory('authInterceptor', ["$injector", "adamaCo
 		// Add authorization token to headers
 		request: function(config) {
 			config.headers = config.headers || {};
-			if (!config.headers['x-auth-token'] && config.url.indexOf(adamaConstant.apiBase) === 0) {
+			if (!config.headers['Authorization'] && config.url.indexOf(adamaConstant.apiBase) === 0) {
 				return getAdamaTokenService().getToken().then(function(token) {
 					if (token) {
 						config.headers['Authorization'] = 'Bearer ' + token;
