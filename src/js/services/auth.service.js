bc892cf87e58313833fbf84598d676e22ad8b794
Vincent Delacourt <vincent.delacourt@adama-development.com>
2016-04-27 21:23:39 +0800

    add api to the url for the refreshUserExternal

--- src/js/services/auth.service.js
+++ src/js/services/auth.service.js
@@ -16,7 +16,7 @@ angular.module('adama-mobile').factory('authService', function($http, $ionicAuth
 			console.log('login is ok, ask custom auth server to refresh the user data');
 			return $http({
 				method: 'POST',
-				url: adamaConstant.apiBase + 'externalLogin/refreshUserExternal',
+				url: adamaConstant.apiBase + 'api/externalLogin/refreshUserExternal',
 				data: {
 					externalId: username
 				}
