45ef30dcd66d151f83c2851ba74c35215b652555
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-04-28 01:41:59 +0800

    change mock according to api

--- mocks/binary-files.js
+++ mocks/binary-files.js
@@ -4,9 +4,11 @@ angular.module('adama-mobile').run(function($httpBackend, $http) {
 	$httpBackend.when('PUT', '/files').respond(function(method, url, data) {
 		data = JSON.parse(data);
 		console.warn('PUT /files', method, url, data);
-		var result = {};
-		angular.forEach(data.idList, function(id) {
-			result[id] = id + '?comingFormMock=true';
+		var result = {
+			urlList : {}
+		};
+		angular.forEach(data, function(id) {
+			result.urlList[id] = id + '?comingFormMock=true';
 		});
 		return [ 200, result ];
 	});
