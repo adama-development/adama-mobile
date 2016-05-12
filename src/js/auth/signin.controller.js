69a357024a0af4332b25b2174f292e3cb69d2ea1
Vincent Delacourt <vincent.delacourt@adama-development.com>
2016-05-12 20:47:27 +0800

    loading message and indicator while signing in

--- src/js/auth/signin.controller.js
+++ src/js/auth/signin.controller.js
@@ -2,7 +2,9 @@
 
 angular.module('adama-mobile').controller('SigninCtrl', function($rootScope, $state, authService, $filter, $ionicPopup) {
 	var ctrl = this;
+	ctrl.loading = false;
 	ctrl.signin = function(userName, userPassword) {
+		ctrl.loading = true;
 		authService.login(userName, userPassword).then(function() {
 			console.log('user is logged in in both ionic and backend, rediret to app.main');
 			$state.go('app.main');
@@ -14,6 +16,8 @@ angular.module('adama-mobile').controller('SigninCtrl', function($rootScope, $st
 				title: translateFn('SIGNIN_ERROR_TITLE'),
 				template: translateFn('SIGNIN_ERROR_MESSAGE')
 			});
+		}).finally(function() {
+			ctrl.loading = false;
 		});
 	};
 });
