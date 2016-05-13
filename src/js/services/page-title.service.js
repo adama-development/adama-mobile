5cfbe18b188611aee8feebfea69db4fb9f3cb8bb
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-05-13 14:08:12 +0800

    translate page title with additional translate values

--- src/js/services/page-title.service.js
+++ src/js/services/page-title.service.js
@@ -4,8 +4,8 @@ angular.module('adama-mobile').factory('pageTitle', function($rootScope, $filter
 	var translateFn = $filter('translate');
 	var api = {};
 
-	api.set = function(pageTitleKey) {
-		var newTitle = translateFn(pageTitleKey);
+	api.set = function(pageTitleKey, data) {
+		var newTitle = translateFn(pageTitleKey, data);
 		$rootScope.pageTitle = newTitle;
 	};
 
