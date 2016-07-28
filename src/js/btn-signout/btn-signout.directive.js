7a916be8fa8af906461ba8fc338fd80bd73a5d40
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-07-28 11:41:52 +0800

    additional class for signout button

--- src/js/btn-signout/btn-signout.directive.js
+++ src/js/btn-signout/btn-signout.directive.js
@@ -2,6 +2,9 @@
 
 angular.module('adama-mobile').component('btnSignout', {
 	templateUrl: 'adama-mobile/btn-signout/btn-signout.html',
+	bindings: {
+		additionalClass: '@'
+	},
 	controller: function(authService, $state) {
 		var ctrl = this;
 		ctrl.signout = function() {
